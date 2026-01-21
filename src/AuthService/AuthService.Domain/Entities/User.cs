5b9f04e111e8e88bc402948ab05377965dd7f8a9
Натали Жук <natalizuk09@gmail.com>
2026-01-22 01:56:10 +0300

    feat(domain): add User with role and email confirmation

---
+++ src/AuthService/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using Users.Domain.Enums;
+
+namespace AuthService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Email { get; private set; } = null!;
+        public string PasswordHash { get; private set; } = null!;
+        public bool EmailConfirmed { get; private set; }
+        public string? EmailConfirmationToken { get; private set; }
+        public Role Role { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        private User() { }
+        public User(string email, string passwordHash)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            PasswordHash = passwordHash;
+            Role = Role.User;
+            EmailConfirmed = false;
+            EmailConfirmationToken = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void ConfirmEmail()
+        {
+            EmailConfirmed = true;
+            EmailConfirmationToken = null;
+        }
+    }
+}
---
+++ src/AuthService/AuthService.Domain/Enums/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Users.Domain.Enums
+{
+    public enum Role
+    {
+        User = 0,
+        Admin = 1
+    }
+}
