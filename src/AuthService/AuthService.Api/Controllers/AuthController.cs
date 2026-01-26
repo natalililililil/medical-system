0694cce322490fe2fe2cee25c3a657b36e023947
Натали Жук <natalizuk09@gmail.com>
2026-01-26 18:12:14 +0300

    feat(api): add account registration endpoint

---
+++ src/AuthService/AuthService.Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using AuthService.Application.Accounts.Commands;
+using AuthService.Application.Accounts.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AuthController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            if (request.Password != request.ConfirmPassword)
+                return BadRequest("Passwords do not match");
+
+            await _mediator.Send(new RegisterAccountCommand(request.Email, request.Password));
+            return Ok();
+        }
+
+    }
+}
--- src/AuthService/AuthService.Api/Program.cs
+++ src/AuthService/AuthService.Api/Program.cs
@@ -9,8 +9,19 @@ builder.Services.AddControllers();
 
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(RegisterAccountCommand).Assembly));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthService API v1");
+    });
+}
 
 app.UseHttpsRedirection();
 
