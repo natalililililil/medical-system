a11874dda62d47e6d3653125238bea8e765cc915
Натали Жук <natalizuk09@gmail.com>
2026-01-27 19:15:39 +0300

    feat(api): add a global exception handling

---
+++ src/AuthService/AuthService.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+
+namespace AuthService.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ValidationException ex)
+            {
+                context.Response.StatusCode = 400;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    message = ex.Message
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    message = "Неожиданная ошибка сервера"
+                });
+            }
+        }
+    }
+}
